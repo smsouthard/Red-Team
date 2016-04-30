4608740876d3c3ab822f02bc4b3b3c0f4d546355
Sean Southard <sean.m.southard@gmail.com>
2016-04-30 16:07:10 -0600

    playing test_read

--- images/processing/test_read.py
+++ images/processing/test_read.py
@@ -1,5 +1,22 @@
 import numpy as np
 from PIL import Image
+from os import walk
+import pandas as pd
+
+
+driver_list = pd.read_csv('/home/smsouthard/Data/train/driver_imgs_list.csv')
+
+mypath = '/home/smsouthard/Data/train/'
+
+driver_list['path'] = mypath \
+                      + driver_list['classname'] \ 
+                      + '/' \
+                      + driver_list['img']
+
+
+
+
+for
 
 numpy.asarray(Image.open('').convert('L'))
 
